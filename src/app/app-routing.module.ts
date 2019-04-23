d5c2f14e83c9dd693a1e8389aee248bc9c235a14
michael-basweti <baswetima@gmail.com>
2019-04-23 20:46:38 +0300

    add default path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,12 @@ import {RegistrationComponent} from './registration/registration.component';
 import {LoginComponent} from './login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component'
 import { HomeComponent } from './components/home/home.component';
-import { from } from 'rxjs';
 
 const routes: Routes = [
   {path: 'registration', component:RegistrationComponent},
   {path: 'login', component:LoginComponent},
   {path: 'dashboard', component:DashboardComponent},
-  {path: 'home', component:HomeComponent},
+  {path: 'home', component:HomeComponent, pathMatch: 'full'},
 
 ];
 
