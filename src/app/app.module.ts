2eb6a60fd363d75f16e04cd12d66a35b2159192f
michael-basweti <baswetima@gmail.com>
2019-05-10 10:48:17 +0300

    add upload from frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
-import { ReactiveFormsModule }    from '@angular/forms';
+import { ReactiveFormsModule, FormsModule, }    from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
@@ -24,6 +24,7 @@ import { HomeComponent } from './components/home/home.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     ReactiveFormsModule,
   ],
   providers: [],
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription, from } from 'rxjs';
 import { first } from 'rxjs/operators';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ProfileService } from '../../services/profile.service';
 import { Profile } from '../../models/profile';
+
+let token = localStorage.getItem('LoggedInUser')
+console.log(`Token ${token}`);
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Authorization': `Token ${token}`,
+  })
+};
 
 @Component({
   selector: 'app-dashboard',
@@ -10,12 +21,19 @@ import { Profile } from '../../models/profile';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  yourForm: FormGroup;
   profile: Profile[] = [];
 
   constructor(
     private profileService: ProfileService,
-  ) { }
+    private fb: FormBuilder,
+    private http: HttpClient
+  ) {
+    this.yourForm = this.fb.group({
+      email: [''],
+      file: ['']
+    });
+  }
 
   ngOnInit() {
     this.loadUserProfile();
@@ -27,4 +45,31 @@ export class DashboardComponent implements OnInit {
         
     });
 }
+uploadDocument(event: any) {
+  if (event.target.files && event.target.files[0]) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.yourForm.get('file').setValue(event.target.files[0]);
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  }
 }
+
+onSubmit(): void {
+  const uploadData = new FormData();
+  uploadData.append('profile', this.yourForm.get('email').value);
+  uploadData.append('image_url', this.yourForm.get('file').value);
+  this.http.post('http://127.0.0.1:8000/profile_files/image/', uploadData, httpOptions)
+  .subscribe(
+    data=>{
+console.log(data);
+
+    },
+    error=>{
+console.log(error);
+
+    }
+    );
+}
+}
+
